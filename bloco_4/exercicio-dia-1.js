107f3b31b8bfb6d15b0d43eb87aca6f72f9589ea
Raphael <raphaellfpg@gmail.com>
2021-03-26 15:49:13 -0300

    Execicio do dia 1 finalizado

--- bloco_4/exercicio-dia-1.js
+++ bloco_4/exercicio-dia-1.js
@@ -102,18 +102,97 @@ switch (chessPiece.toLowerCase()) {
 let percent = Math.ceil(Math.random() * 100);
 
 if (percent >= 90 && percent <= 100) {
-    console.log('A')
+    console.log('A');
 }else if (percent >= 80 && percent <= 100) {
-    console.log('B')
+    console.log('B');
 }else if (percent >= 70 && percent <= 100) {
-    console.log('C')
+    console.log('C');
 }else if (percent >= 60 && percent <= 100) {
-    console.log('D')
+    console.log('D');
 }else if (percent >= 50 && percent <= 100) {
-    console.log('E')
+    console.log('E');
 }else if (percent < 50 && percent >= 0) {
-    console.log('F')
+    console.log('F');
 }else {
-    console.log('Error')
+    console.log('Error');
 }
 
+// Exercicio 8
+
+let num1 = 2;
+let num2 = 3;
+let num3 = 2;
+
+if(num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+// Exercicio 9
+
+if(num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+// Exercico 10
+
+let cost = 100;
+let saleValue = 150;
+let tax = cost * 0.2;
+let totalCost = cost + tax;
+let gain = saleValue - totalCost;
+
+if(totalCost < 0 || saleValue < 0) {
+    console.log('Erro');
+}else{
+    console.log('Meu lucro foi: ' + gain + '.')
+}
+
+// Exercicio 11
+
+let salary = 8000;
+let inss;
+let ir;
+
+
+if (salary <= 1556.94) {
+    inss = salary - (salary * 0.08);
+    console.log(inss);
+
+}else if (salary > 1556.94 && salary <= 2594.92) {
+    inss = salary - (salary * 0.09)
+    console.log(inss)
+
+}else if (salary > 2594.92 && salary <= 5189.82) {
+    inss = salary - (salary * 0.11)
+    console.log(inss);
+
+}else {
+    inss = salary - 570.88;
+    console.log(inss)
+}
+
+let netSalary;
+
+if(inss <= 1903.98) {
+    netSalary = inss
+    console.log(netSalary)
+}else if (inss > 1903.98 && inss <= 2826.65) {
+    netSalary = inss - ((inss * 0.075) - 142.8);
+    console.log(netSalary);
+
+}else if (inss > 2826.65 && inss <= 3751.05) {
+    netSalary = inss - ((inss * 0.15) - 354.8);
+    console.log(netSalary);
+
+}else if (inss > 3751.05 && inss <= 4664.68) {
+    netSalary = inss - ((inss * 0.225) - 636.13);
+    console.log(netSalary);
+
+}else {
+    netSalary = inss - ((inss * 0.275) - 869.36);
+    console.log('Meu salário liquido é: ' + netSalary);
+}
